768a5473c359942f3849223375ffbe38a7ca43c7
Rabi <singamayumrabiamal@gmail.com>
2026-02-02 20:45:14 +0530

    git restore

---
+++ LAST/Javascript/first.js
@@ -0,0 +1,44 @@
+// Data Type: 
+// 1. Number
+// 2. String
+// 3. Boolean
+
+let num1 = 10;
+let num2 = 20;
+let name = "JavaScript";
+let isOkay = true;
+
+
+// operation
+let a = 40;
+let b = 30;
+
+let add = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+let rem = a % b;
+
+// console.log(add);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+// console.log(rem);
+
+
+let x = 50;
+let y = 15;
+
+let remainder = x % y;
+// console.log(remainder);
+
+
+// if statement
+let condition = 61%3 == 0;
+
+if (condition) {
+  console.log("true"); 
+  
+} else {
+  console.log("false"); 
+}
